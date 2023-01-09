4434260d53f25e11021eeb5a4d7b2c1b9c872c68
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2023-01-10 00:50:06 +0100

    organize files

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_best_food/app/cubit/root_cubit.dart';
 import 'package:my_best_food/features/auth/login_page/login_page.dart';
+import 'package:my_best_food/features/home_page/home_page.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,11 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'myBestFood',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RootPage(),
+      home: const RootPage(),
     );
   }
 }
@@ -24,15 +26,17 @@ class RootPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          final user = snapshot.data;
+    return BlocProvider(
+      create: (context) => RootCubit()..start(),
+      child: BlocBuilder<RootCubit, RootState>(
+        builder: (context, state) {
+          final user = state.user;
           if (user == null) {
             return LoginPage();
           }
-          // TODO create Home Page
-          return LoginPage();
-        });
+          return const HomePage();
+        },
+      ),
+    );
   }
 }
