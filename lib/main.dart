e0ba0a67d94a7bf2eb0afecc15ce3bfb517cb9f2
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2023-01-09 19:40:17 +0100

    configure flutterfire

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_best_food/firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
 
 class MyApp extends StatelessWidget {
