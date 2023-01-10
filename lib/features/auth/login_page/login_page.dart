d0bccfe3b32f166195dc2407e5ca6c56a56080d2
Piotr Pęczkowski <peczkowski.piotrek@gmail.com>
2023-01-10 18:47:17 +0100

    create and configure login screen

--- lib/features/auth/login_page/login_page.dart
+++ lib/features/auth/login_page/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -15,6 +16,66 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text('Zaloguj się'),
+              ),
+              // EMAIL TextField
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                ),
+                child: TextField(
+                  controller: widget.emailController,
+                  decoration: const InputDecoration(hintText: 'E-mail'),
+                ),
+              ),
+              // PASSWORD TextField
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 10,
+                ),
+                child: TextField(
+                  controller: widget.passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(hintText: 'Hasło'),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 20,
+                ),
+                child: TextButton(
+                  onPressed: () async {
+                    try {
+                      await FirebaseAuth.instance.signInWithEmailAndPassword(
+                        email: widget.emailController.text,
+                        password: widget.passwordController.text,
+                      );
+                    } catch (error) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          duration: const Duration(seconds: 4),
+                          backgroundColor: Colors.red,
+                          content: Text('$error'),
+                        ),
+                      );
+                    }
+                  },
+                  child: const Text('Zaloguj'),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
